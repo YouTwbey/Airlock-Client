a6c05ea44aef0f66e26bb18dbabf654ed91e8f22
Carsten23455 <carstencorbin2008@gmail.com>
2026-04-16 05:02:30 -0500

    silencer perma mutes someone now...

---
+++ Silencer.cs
@@ -0,0 +1,83 @@
+﻿using AirlockClient.Attributes;
+using AirlockClient.Managers.Gamemode;
+using Il2CppSG.Airlock;
+using Il2CppSG.Airlock.Roles;
+using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AirlockClient.Data.Roles.MoreRoles.Imposter
+{
+    public class Silencer : SubRole
+    {
+        public static SubRoleData Data = new SubRoleData
+        {
+            Name = "Silencer",
+            RoleType = "Imposter",
+            Description = "Mute others",
+            AC_Description = "Mute one other person for the rest of the game",
+            Team = GameTeam.Impostor,
+            Amount = 0
+        };
+
+        void Start()
+        {
+            MoreRolesManager.QueueRoleDisplay(PlayerWithRole, this, Data);
+        }
+
+        public bool CanMutePlayer = true;
+        public PlayerState PlayerToMute = null;
+        public GameRole OriginalRole = GameRole.NotSet;
+
+        public override void OnAllVotesCast()
+        {
+            if (PlayerToMute != null && PlayerToMute.IsSpectating == true && OriginalRole != GameRole.NotSet && PlayerToMute.IsAlive && PlayerToMute.IsConnected)
+            {
+                PlayerToMute.IsSpectating = false;
+                MoreRolesManager.Killing.AlterRole(OriginalRole, PlayerToMute.PlayerId);
+
+                MelonCoroutines.Start(DelaySpectatingForTaskState());
+            }
+        }
+
+        public override void OnVotingBegan(PlayerState bodyReported, PlayerState reportingPlayer)
+        {
+            if (PlayerToMute != null && PlayerToMute.IsAlive && PlayerToMute.IsConnected)
+            {
+                PlayerToMute.IsSpectating = false;
+
+                MelonCoroutines.Start(DelaySpectator());
+            }
+        }
+
+        public override void OnPlayerEjected(PlayerState ejectedPlayer, GameRole role)
+        {
+            
+        }
+
+        public IEnumerator DelaySpectator()
+        {
+            yield return new WaitForSeconds(2f);
+
+            PlayerToMute.IsSpectating = true;
+        }
+
+        public IEnumerator DelaySpectatingForTaskState()
+        {
+            yield return new WaitForSeconds(5);
+
+            PlayerToMute.IsSpectating = true;
+        }
+
+        public override void OnGameEnd(GameTeam teamThatWon)
+        {
+            PlayerToMute.IsSpectating = false;
+            PlayerToMute = null;
+        }
+    }
+}
---
+++ VotePatch.cs
@@ -0,0 +1,87 @@
+﻿using AirlockAPI.Data;
+using AirlockClient.AC;
+using HarmonyLib;
+using Il2CppFusion;
+using Il2CppSG.Airlock;
+using UnityEngine;
+
+namespace AirlockClient.Patches
+{
+    [HarmonyPatch()]
+    public class VotePatch
+    {
+        [HarmonyPatch(typeof(VoteManager), nameof(VoteManager.RPC_Vote), new System.Type[] { typeof(PlayerRef), typeof(PlayerRef), typeof(RpcInfo) })]
+        [HarmonyPrefix]
+        public static bool Prefix1(PlayerRef voteAgainstPlayer, PlayerRef sourcePlayer, RpcInfo info)
+        {
+            PlayerState voter = GameObject.Find("PlayerState (" + voteAgainstPlayer.PlayerId + ")").GetComponent<PlayerState>();
+            PlayerState voted = GameObject.Find("PlayerState (" + sourcePlayer.PlayerId + ")").GetComponent<PlayerState>();
+
+            if (CurrentMode.IsHosting && !CurrentMode.Modded)
+            {
+                if (!AntiCheat.Instance.VerifyVote(voter, voted, info))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(VoteManager), nameof(VoteManager.RPC_Vote), new System.Type[] { typeof(PlayerRef), typeof(RpcInfo) })]
+        [HarmonyPrefix]
+        public static bool Prefix2(PlayerRef sourcePlayer, RpcInfo info)
+        {
+            PlayerState voter = GameObject.Find("PlayerState (" + sourcePlayer.PlayerId + ")").GetComponent<PlayerState>();
+
+            if (CurrentMode.IsHosting && !CurrentMode.Modded)
+            {
+                if (!AntiCheat.Instance.VerifyVote(voter, null, info))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch()]
+    public class CallVote
+    {
+        [HarmonyPatch(typeof(VoteManager), nameof(VoteManager.RPC_CallVote), new System.Type[] { typeof(int), typeof(PlayerRef), typeof(NetworkBool), typeof(RpcInfo) })]
+        [HarmonyPrefix]
+        public static bool Prefix1(int foundPlayer, PlayerRef sourcePlayer, NetworkBool forceVote, RpcInfo info)
+        {
+            PlayerState caller = GameObject.Find("PlayerState (" + sourcePlayer.PlayerId + ")").GetComponent<PlayerState>();
+            PlayerState bodyFound = GameObject.Find("PlayerState (" + foundPlayer + ")").GetComponent<PlayerState>();
+
+            if (CurrentMode.IsHosting && !CurrentMode.Modded)
+            {
+                if (!AntiCheat.Instance.VerifyBodyReport(caller, bodyFound, info))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(VoteManager), nameof(VoteManager.RPC_CallVote), new System.Type[] { typeof(PlayerRef), typeof(NetworkBool), typeof(RpcInfo) })]
+        [HarmonyPrefix]
+        public static bool Prefix(PlayerRef sourcePlayer, NetworkBool forceVote, RpcInfo info)
+        {
+            PlayerState caller = GameObject.Find("PlayerState (" + sourcePlayer.PlayerId + ")").GetComponent<PlayerState>();
+
+            if (CurrentMode.IsHosting && !CurrentMode.Modded)
+            {
+                if (!AntiCheat.Instance.VerifyMeeting(caller, info))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
